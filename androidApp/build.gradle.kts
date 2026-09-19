ec1e79525374871048a8eb758ad6cf5339b5c210
noorulakbar <noorulakbar32@gmail.com>
2026-09-19 17:41:38 +0530

    chore: bump version to 2.1 (versionCode 3)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -54,8 +54,8 @@ android {
         applicationId = "com.example.ruwia"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "2.1"
         manifestPlaceholders["appName"] = "Ruwia"
     }
     packaging {
